1b2c540ab92e42674e67176fd5b227bb834f8162
Neverfinsh <947395491@qq.com>
2017-05-16 18:23:42 +0800

    测试提交GitHub

---
+++ TestGitHub/src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Lw  primary
+ * 2017/5/16
+ * 功能描述：
+ * 注意点：
+ * 后期重构：
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    	}
+}
