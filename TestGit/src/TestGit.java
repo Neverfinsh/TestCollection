b5aa23ea1821a20ae2927c434bebb6dd0078bc68
Neverfinsh <947395491@qq.com>
2017-05-16 18:20:32 +0800

    测试提交GitHub

---
+++ TestGit/src/TestGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Lw  primary
+ * 2017/5/16
+ * 功能描述：
+ * 注意点：
+ * 后期重构：
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("测试git，发布git项目");
+    	}
+}
